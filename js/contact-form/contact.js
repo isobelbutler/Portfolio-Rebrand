8802f2f14131612f7afc59bc4c43569b9148c027
isobelbutler <isobutler@icloud.com>
2023-07-14 10:52:05 +0100

    Added sendMail function

--- js/contact-form/contact.js
+++ js/contact-form/contact.js
@@ -1,1 +1,20 @@
+let baseURL = 'http://127.0.0.1:5500/index.html';
+
 console.log('Contact script running');
+
+const form = document.getElementById('contact-form');
+
+const formEvent = form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let email = new FormData(form);
+    sendMail(email);
+})
+
+const sendMail = (email) => {
+    fetch(`${baseURL}/send`, {
+        method: 'POST',
+        body: email,
+    }).then((response) => {
+        return response.json();
+    });
+};
