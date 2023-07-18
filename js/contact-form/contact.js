fb9289f25aeb72a6ea7bd59608882e0b439ca430
isobelbutler <isobutler@icloud.com>
2023-07-18 11:15:27 +0100

    Testing form

--- js/contact-form/contact.js
+++ js/contact-form/contact.js
@@ -1,29 +1,29 @@
-// const closeButton = document.getElementById('close-submit-popup');
-// const form = document.getElementById('contact-form');
-// const popup = document.getElementById('popup');
-// let userEmail = document.getElementById('form-email');
-// let userName = document.getElementById('form-name');
-// let userMessage = document.getElementById('form-message');
+const closeButton = document.getElementById('close-submit-popup');
+const form = document.getElementById('contact-form');
+const popup = document.getElementById('popup');
+let userEmail = document.getElementById('form-email');
+let userName = document.getElementById('form-name');
+let userMessage = document.getElementById('form-message');
 
-// closeButton.addEventListener('click', closePopup);
+closeButton.addEventListener('click', closePopup);
 
-// form.addEventListener('submit', openPopup);
+form.addEventListener('submit', openPopup);
 
-// function openPopup() {
-//   // Prevent the default form submission
-//   //   event.preventDefault();
+function openPopup() {
+  // Prevent the default form submission
+  //   event.preventDefault();
 
-//   // Show the popup
-//   popup.style.display = 'flex';
-// }
+  // Show the popup
+  popup.style.display = 'flex';
+}
 
-// function closePopup() {
-//   resetForm();
-//   popup.style.display = 'none';
-// }
+function closePopup() {
+  resetForm();
+  popup.style.display = 'none';
+}
 
-// function resetForm() {
-//   userEmail.value = '';
-//   userName.value = '';
-//   userMessage.value = '';
-// }
+function resetForm() {
+  userEmail.value = '';
+  userName.value = '';
+  userMessage.value = '';
+}
